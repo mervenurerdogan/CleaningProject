1f87967563a0727aeb0b304e7ffbaf3541ff3b1c
mervenurerdogan <merveeenur2@gmail.com>
2023-04-26 23:21:19 +0300

    Add project files.

---
+++ CleaningProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CleaningProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
