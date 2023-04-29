9e0848b7f519d720c82166c4aaacbcaccfd54878
mervenurerdogan <merveeenur2@gmail.com>
2023-04-29 23:19:22 +0300

    service abstract classlar eklendi

---
+++ CleaningProjectBusiness/Abstract/IBaseService.cs
@@ -0,0 +1,19 @@
+﻿using CleaningProjectCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningProjectBusiness.Abstract
+{
+    public interface IBaseService<Tablo> where Tablo : class,IEntity,new()
+    {
+
+        void Add(Tablo tablo);
+        void Update(Tablo tablo);
+        void Delete(Tablo tablo);
+        Tablo GetById(int id);
+        List<Tablo> GetAll();   
+    }
+}
