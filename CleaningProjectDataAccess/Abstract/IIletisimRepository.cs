946db8bc1a285cb9c88c1dd2a12158848423ff29
mervenurerdogan <merveeenur2@gmail.com>
2023-04-30 18:32:11 +0300

    addd repository ileitiism sosyal medya ve hakkimizda

---
+++ CleaningProjectDataAccess/Abstract/IIletisimRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleaningProjectModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningProjectDataAccess.Abstract
+{
+    public interface IIletisimRepository:IEntityRepository<Iletisim>
+    {
+
+    }
+}
---
+++ CleaningProjectDataAccess/Concrete/EfHakkimizdaRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleaningProjectDataAccess.Abstract;
+using CleaningProjectModel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningProjectDataAccess.Concrete
+{
+    public class EfHakkimizdaRepository:EfEntityRepository<Hakkimizda>,IHakkimizdaRepository
+    {
+
+    }
+}
